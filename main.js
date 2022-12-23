f63783c2131f24e512ee3833c47379cda9a6490b
Asal <Asal@gmail.com>
2022-12-23 15:37:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var str = +prompt('Введите первое число:');
+console.log(str);
+var tjhj = +prompt('Введите второе число:');
+console.log(tjhj);
+var dggg = +prompt('Введите третье число:');
+console.log(dggg);
+// var dgdd = prompt('Cреднее арифметическое:');
+let result = (str + tjhj + dggg) / 3
+
+alert(' Cреднее арифметическое: ' + result)
+
+
+
+
